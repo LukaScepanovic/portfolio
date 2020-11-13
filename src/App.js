677c709af8ebf36b1c1687c8ef6f1cc23f468d85
Luka <scepanovic.luka.97@gmail.com>
2020-11-13 16:57:40 +0100

    css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from 'react'
+import './css/app.css'
+
 function App() {
-  return "Hi there you pretty monsta"
+  return (
+    <div className="title">
+      <h1>Hi there you pretty monsta</h1>
+    </div>
+
+  )
 }
 
 export default App;
