e07c4138ddccaf7951c9284141d7d877663cf102
Luka <scepanovic.luka.97@gmail.com>
2020-11-20 18:21:45 +0100

    netlify form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,26 +1,29 @@
 import React from 'react'
 
 export default function ContactForm() {
-    return (
-        <section className="contactme" id="contactme">
-        <h1 className="title">Contact Me</h1>
-        <form name="contact" method="POST" data-netlify="true">
-          <div className="wrapper">
-            <div className="contact-form">
-              <div className="input-fields">
-                <input type="text" name="name" className="input" placeholder="Name" />
-                <input type="text" name="email" className="input" placeholder="Email" />
-                <input type="text" name="phone" className="input" placeholder="Phone" />
-                <input type="text" name="subject" className="input" placeholder="Subject" />
-              </div>
-              <div className="msg">
-                <textarea placeholder="Message" defaultValue={""} />
-                <div className="btn-primary">
-                  <button>Submit</button>
-                </div>
+  return (
+    <section className="contactme" id="contactme">
+      <h1 className="title">Contact Me</h1>
+
+      <form name="contact" action="/contact" method="post">
+        <div className="wrapper">
+          <div className="contact-form">
+            <div className="input-fields">
+              <input type="hidden" name="form-name" value="contact"/>
+              <input required type="text" name="name" className="input" placeholder="Name" />
+              <input required type="text" name="email" className="input" placeholder="Email" />
+              <input type="text" name="phone" className="input" placeholder="Phone" />
+              <input type="text" name="subject" className="input" placeholder="Subject" />
+            </div>
+            <div className="msg">
+              <textarea required name="message" placeholder="Message" ></textarea>/
+              <div className="btn-primary">
+                <button type="submit">Submit</button>
               </div>
             </div>
-          </div></form>
-      </section>
-    )
+          </div>
+        </div>
+      </form>
+    </section>
+  )
 }
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,26 +1,34 @@
 import React from 'react'
-import { Tabs } from 'antd';
+import { Tabs, Row, Col } from 'antd';
 
 const { TabPane } = Tabs;
 
-function callback(key) {
-  console.log(key);
-}
 
 export default function Experience() {
-    return (
+  return (
+    <>
+    <Row align="center">
+      <Col>
+      <h1>Work experience</h1>
+      </Col>
+    </Row>
+    <Row align="center">
+      <Col>
         <div className="experience">
-        <Tabs defaultActiveKey="1" onChange={callback}>
-        <TabPane tab="Tab 1" key="1">
-          Content of Tab Pane 1
+          <Tabs defaultActiveKey="1" style={{'fontSize':'25px'}}>
+            <TabPane tab="Tab 1" key="1">
+              Content of Tab Pane 1
         </TabPane>
-        <TabPane tab="Tab 2" key="2">
-          Content of Tab Pane 2
+            <TabPane tab="Tab 2" key="2">
+              Content of Tab Pane 2
         </TabPane>
-        <TabPane tab="Tab 3" key="3">
-          Content of Tab Pane 3
+            <TabPane tab="Tab 3" key="3">
+              Content of Tab Pane 3
         </TabPane>
-      </Tabs>
-      </div>
-    )
+          </Tabs>
+        </div>
+      </Col>
+    </Row>
+    </>
+  )
 }
